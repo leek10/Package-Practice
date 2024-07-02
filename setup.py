649b2960ad6418309a736235bd04f5872a7133d2
Kayla Lee <kl2jmx@virginia.edu>
2024-07-02 10:59:50 -0400

    Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'practice-package',
+      version = '0.1',
+      description = 'A simple demonstration package',
+      url = 'https://github.com/leek10/package-practice',
+      author = 'Kayla Lee',
+      author_email = 'MIT',
+      packages = ['mypackage'])
